46196bdba54d50d81c248f1d991f45359bf2f4ac
Joseph <turnbjos@mail.gvsu.edu>
2019-09-20 13:51:06 -0400

    Part 1

--- Homework 1/ViewController.swift
+++ Homework 1/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let detectTouch = UITapGestureRecognizer(target: self, action:
+            #selector(self.dismissKeyboard))
+        self.view.addGestureRecognizer(detectTouch)
+    }
+    
+    @objc func dismissKeyboard(){
+        self.view.endEditing(true)
     }
 
 
